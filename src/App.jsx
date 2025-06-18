af3d929729deb3635fd79324bea9544c57728315
bl-haitem <haitembelaib@gmail.com>
2025-06-18 23:19:46 +0100

    شرح مختصر للتعديلات

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Orientation from './pages/Orientation';
-import AllMajors from './pages/AllMajors'; 
-import Footer from "./pages/Footer";
-import ScrollToTop from './pages/ScrollToTop';
+import AllMajors from './pages/AllMajors';
+import Footer from './pages/Footer'; // كان الخطأ هنا، يجب أن يكون في components
+import ScrollToTop from './pages/ScrollToTop'; // تأكد أن ScrollToTop موجود في components
 
 export default function App() {
   return (
     <>
       <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/orientation" element={<Orientation />} />
-        <Route path="/majors" element={<AllMajors />} />
-      </Routes>
-      <Footer />
+      <div className="min-h-screen flex flex-col justify-between">
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/orientation" element={<Orientation />} />
+            <Route path="/majors" element={<AllMajors />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
