8acad721ffb11f0de2bd9a372dcda35010f0c7f2
Belaib Haitem <haitembelaib@gmail.com>
2025-06-18 23:36:51 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,23 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Orientation from './pages/Orientation';
-import AllMajors from './pages/AllMajors';
-import Footer from './pages/Footer'; // كان الخطأ هنا، يجب أن يكون في components
-import ScrollToTop from './pages/ScrollToTop'; // تأكد أن ScrollToTop موجود في components
+import AllMajors from './pages/AllMajors'; 
+import Footer from "./pages/Footer";
+import ScrollToTop from './pages/ScrollToTop';
+import { Analytics } from '@vercel/analytics/react'; // ✅ إضافة هذه السطر
 
 export default function App() {
   return (
     <>
       <ScrollToTop />
-      <div className="min-h-screen flex flex-col justify-between">
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/orientation" element={<Orientation />} />
-            <Route path="/majors" element={<AllMajors />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/orientation" element={<Orientation />} />
+        <Route path="/majors" element={<AllMajors />} />
+      </Routes>
+      <Footer />
+      <Analytics /> {/* ✅ إضافة هذه السطر */}
     </>
   );
 }
+
